de394fd8c7c06e495caad81d964314306779b8d5
Benjamin <bj10@live.fr>
2024-02-28 11:04:13 +0100

    Mise à jour Final

--- modal.js
+++ modal.js
@@ -11,37 +11,35 @@ function editNav() {
 // MODALS DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
 const closeModal = document.querySelectorAll(".close");
 const content = document.querySelector(".content");
-const testmodal = document.querySelector(".postRegisterModal");
-const testm = document.querySelectorAll(".testm");
-const testcontent = document.querySelector(".postRegisterContent");
-const testclose = document.querySelector(".postRegisterClose");
-const btnfermer = document.querySelector(".btn-fermer");
+const postRegisterModal = document.querySelector(".postRegisterModal");
+const postRegisterClose = document.querySelector(".postRegisterClose");
+const btnFermer = document.querySelector(".btn-fermer");
 
-//  -------------- POST REGISTRATION MODAL --------------
+// -------------- POST REGISTRATION MODAL --------------
 
 // event listeners post registration modal
-testm.forEach((btn) => btn.addEventListener("click", launchM));
-testclose.addEventListener("click", handleM); // Use addEventListener directly
-btnfermer.addEventListener("click", handleM);
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+postRegisterClose.addEventListener("click", handleClosePostRegisterModal);
+btnFermer.addEventListener("click", handleClosePostRegisterModal);
 
 // launch post registration modal
-function launchM() {
-  testmodal.style.display = "block";
-  testcontent.classList.remove("hide-modal");
+function launchModal() {
+  modalbg.style.display = "block";
+  content.classList.remove("hide-modal");
 }
 
-// close  post registration modal
-function handleM() {
-  testcontent.classList.add("hide-modal");
+// close post registration modal
+function handleClosePostRegisterModal() {
+  content.classList.add("hide-modal");
   setTimeout(function () {
-    testmodal.style.display = "none";
+    modalbg.style.display = "none";
   }, 222); // timeout for a smoother effect
+  postRegisterModal.style.display = "none";
 }
 
-//  -------------- REGISTRATION MODAL --------------
+// -------------- REGISTRATION MODAL --------------
 
 // event listeners
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -58,16 +56,82 @@ function launchModal() {
 // close modal form
 function handleCloseModal() {
   content.classList.add("hide-modal");
-  // Listen for the animation end event to actually hide the modal after the animation completes
-  content.addEventListener("animationend", function (event) {
-    if (event.animationName === "modalclose") {
-      modalbg.style.display = "none";
-      content.classList.remove("hide-modal");
-    }
-  });
+  setTimeout(function () {
+    modalbg.style.display = "none";
+  }, 222); // timeout for a smoother effect
 }
 
-// FORM VALIDATION 
+// FORM VALIDATION PROCESS
+
+const firstNameInput = document.getElementById("first");
+const firstNameError = document.getElementById("firstNameError");
+const lastNameInput = document.getElementById("last");
+const lastNameError = document.getElementById("lastNameError");
+const emailInput = document.getElementById("email");
+const emailError = document.getElementById("emailError");
+const birthdateInput = document.getElementById("birthdate");
+const birthdateError = document.getElementById("birthdateError");
+const checkbox = document.getElementById("checkbox1");
+const checkboxError = document.getElementById("checkboxError");
+const quantityInput = document.getElementById("quantity");
+const quantityError = document.getElementById("quantityError");
+const locationInputs = document.querySelectorAll('input[name="location"]');
+const locationError = document.getElementById("locationError");
+
+// Ajoutez une variable pour suivre si le formulaire a été soumis ou non
+let formSubmitted = false;
+
+// EVENTS LISTENERS ON CHANGE
+firstNameInput.addEventListener("input", function () {
+  if (formSubmitted) {
+    validateFirstName();
+  }
+});
+
+firstNameInput.addEventListener("focus", function () {
+  // Réinitialiser l'erreur lorsqu'on se concentre sur le champ
+  firstNameError.style.display = "none";
+  firstNameInput.classList.remove("field-error");
+});
+
+firstNameInput.addEventListener("blur", function () {
+  // Valider le champ lorsqu'on le quitte
+  if (formSubmitted) {
+    validateFirstName();
+  }
+});
+
+lastNameInput.addEventListener("input", function () {
+  if (formSubmitted) {
+    validateName(lastNameInput, lastNameError);
+  }
+});
+
+emailInput.addEventListener("input", function () {
+  if (formSubmitted) {
+    validateEmail(emailInput, emailError);
+  }
+});
+
+birthdateInput.addEventListener("input", function () {
+  if (formSubmitted) {
+    validateBirthdate(birthdateInput, birthdateError);
+  }
+});
+
+quantityInput.addEventListener("input", function () {
+  if (formSubmitted) {
+    validateQuantity(quantityInput, quantityError);
+  }
+});
+
+locationInputs.forEach((input) => {
+  input.addEventListener("input", function () {
+    if (formSubmitted) {
+      validateLocation(locationInputs, locationError);
+    }
+  });
+});
 
 function validateName(input, errorElement) {
   const regex = /^[A-Za-zÀ-ÖØ-öø-ÿ]{2,}$/;
@@ -78,6 +142,19 @@ function validateName(input, errorElement) {
   } else {
     errorElement.style.display = "none";
     input.classList.remove("field-error");
+    return true;
+  }
+}
+
+function validateFirstName() {
+  const regex = /^[A-Za-zÀ-ÖØ-öø-ÿ]{2,}$/;
+  if (!regex.test(firstNameInput.value)) {
+    firstNameError.style.display = "block";
+    firstNameInput.classList.add("field-error");
+    return false;
+  } else {
+    firstNameError.style.display = "none";
+    firstNameInput.classList.remove("field-error");
     return true;
   }
 }
@@ -156,53 +233,14 @@ function validateLocation(locationInputs, errorElement) {
   }
 }
 
-
-// FORM VALIDATION PROCESS
-
-const firstNameInput = document.getElementById("first");
-const firstNameError = document.getElementById("firstNameError");
-const lastNameInput = document.getElementById("last");
-const lastNameError = document.getElementById("lastNameError");
-const emailInput = document.getElementById("email");
-const emailError = document.getElementById("emailError");
-const birthdateInput = document.getElementById("birthdate");
-const birthdateError = document.getElementById("birthdateError");
-const checkbox = document.getElementById("checkbox1");
-const checkboxError = document.getElementById("checkboxError");
-const quantityInput = document.getElementById("quantity");
-const quantityError = document.getElementById("quantityError");
-const locationInputs = document.querySelectorAll('input[name="location"]');
-const locationError = document.getElementById("locationError");
-
-//EVENTS LISTENERS ON CHANGE
-firstNameInput.addEventListener("input", function () {
-  validateName(firstNameInput, firstNameError);
-});
-lastNameInput.addEventListener("input", function () {
-  validateName(lastNameInput, lastNameError);
-});
-emailInput.addEventListener("input", function () {
-  validateEmail(emailInput, emailError);
-});
-birthdateInput.addEventListener("input", function () {
-  validateBirthdate(birthdateInput, birthdateError);
-});
-quantityInput.addEventListener("input", function () {
-  validateQuantity(quantityInput, quantityError);
-});
-locationInputs.forEach((input) => {
-  input.addEventListener("input", function () {
-    validateLocation(locationInputs, locationError);
-  });
-});
-
 function validate() {
-  const firstNameValid = validateName(firstNameInput, firstNameError);
+  const firstNameValid = validateFirstName();
   const lastNameValid = validateName(lastNameInput, lastNameError);
   const emailValid = validateEmail(emailInput, emailError);
   const birthdateValid = validateBirthdate(birthdateInput, birthdateError);
   const quantityValid = validateQuantity(quantityInput, quantityError);
   const locationValid = validateLocation(locationInputs, locationError);
+  const checkboxValid = validateCheckbox(checkbox, checkboxError);
 
   const isValid =
     firstNameValid &&
@@ -210,10 +248,10 @@ function validate() {
     emailValid &&
     birthdateValid &&
     quantityValid &&
-    locationValid;
+    locationValid &&
+    checkboxValid;
 
   if (isValid) {
-    launchM();
     event.preventDefault();
     handleCloseModal();
     return true;
@@ -221,3 +259,5 @@ function validate() {
     return false;
   }
 }
+
+
